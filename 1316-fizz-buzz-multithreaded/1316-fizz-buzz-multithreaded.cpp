ca7958850e6800f653ca60e3b4d1c8c80faf457f
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-04-21 11:11:38 -0600

    Time: 0 ms (100%), Space: 8.3 MB (93.32%) - LeetHub

--- 1316-fizz-buzz-multithreaded/1316-fizz-buzz-multithreaded.cpp
+++ 1316-fizz-buzz-multithreaded/1316-fizz-buzz-multithreaded.cpp
@@ -1,74 +1,158 @@
+// ***************************************************************
+//
+// Mutex implementation, multi core processor
+//
+// ***************************************************************
 class FizzBuzz {
 private:
     int n;
-    std::atomic <int> mAtomicInt{1};
+    int mCurrNum;
+    std::condition_variable mCondVar;
+    std::mutex mMutex;
 
 public:
-    FizzBuzz(int n) {
+    FizzBuzz( int n ) 
+    {
         this->n = n;
+        this->mCurrNum = 1;
     }
 
     // printFizz() outputs "fizz".
-    void fizz(function<void()> printFizz) {
-        while ( mAtomicInt <= n )
+    void fizz( function<void()> printFizz ) 
+    {
+        while ( true ) 
         {
-            if ( mAtomicInt % 3 == 0 && mAtomicInt % 5 != 0 )
-            {
-                printFizz();
-                mAtomicInt += 1;
-            }
-            else
-            {
-                std::this_thread::yield();
-            }
+            unique_lock<mutex> lock( mMutex );
+            mCondVar.wait( lock, [ & ]{ return ( mCurrNum % 3 == 0 && mCurrNum % 5 != 0) || mCurrNum > n; } );
+            if ( mCurrNum > n ) return;
+            printFizz();
+            ++mCurrNum;
+            mCondVar.notify_all();
         }
-    }
+    } // end of fizz
 
     // printBuzz() outputs "buzz".
-    void buzz(function<void()> printBuzz) {
-        while ( mAtomicInt <= n )
+    void buzz( function<void()> printBuzz ) 
+    {
+        while ( true ) 
         {
-            if ( mAtomicInt % 3 != 0 && mAtomicInt % 5 == 0 )
-            {
-                printBuzz();
-                mAtomicInt += 1;
-            }
-            else
-            {
-                std::this_thread::yield();
-            }
+            unique_lock<mutex> lock( mMutex );
+            mCondVar.wait( lock, [ & ]{ return ( mCurrNum % 3 != 0 && mCurrNum % 5 == 0) || mCurrNum > n; } );
+            if ( mCurrNum > n ) return;
+            printBuzz();
+            ++mCurrNum;
+            mCondVar.notify_all();
         }
-    }
+    } // end of buzz
 
     // printFizzBuzz() outputs "fizzbuzz".
-	void fizzbuzz(function<void()> printFizzBuzz) {
-        while ( mAtomicInt <= n )
+	void fizzbuzz( function<void()> printFizzBuzz ) 
+    {
+        while ( true ) 
         {
-            if ( mAtomicInt % 15 == 0 )
-            {
-                printFizzBuzz();
-                mAtomicInt += 1;
-            }
-            else
-            {
-                std::this_thread::yield();
-            }
+            unique_lock<mutex> lock( mMutex );
+            mCondVar.wait( lock, [ & ]{ return ( mCurrNum % 3 == 0 && mCurrNum % 5 == 0) || mCurrNum > n; } );
+            if ( mCurrNum > n ) return;
+            printFizzBuzz();
+            ++mCurrNum;
+            mCondVar.notify_all();
         }
-    }
+    } // end of fizzbuzz
 
     // printNumber(x) outputs "x", where x is an integer.
-    void number(function<void(int)> printNumber) {
-        while ( mAtomicInt <= n )
+    void number( function<void( int )> printNumber ) 
+    {
+        while ( true ) 
         {
-            if ( mAtomicInt % 3 != 0 && mAtomicInt % 5 != 0 )
-            {
-                printNumber( mAtomicInt );
-                mAtomicInt += 1;
-            }
-            else
-            {
-                std::this_thread::yield();
-            }
+            unique_lock<mutex> lock( mMutex );
+            mCondVar.wait( lock, [ & ]{ return ( mCurrNum % 3 != 0 && mCurrNum % 5 != 0) || mCurrNum > n; } );
+            if ( mCurrNum > n ) return;
+            printNumber( mCurrNum );
+            ++mCurrNum;
+            mCondVar.notify_all();
         }
-    }
-};
+    } // end of number
+}; // end of Solution class
+
+// ***************************************************************
+//
+// Atomic integer implementation, single core processor
+//
+// ***************************************************************
+// class FizzBuzz {
+// private:
+//     int n;
+//     std::atomic <int> mAtomicInt{1};
+
+// public:
+//     FizzBuzz( int n ) {
+//         this->n = n;
+//     }
+
+//     // printFizz() outputs "fizz".
+//     void fizz( function<void()> printFizz ) {
+//         // Printing different outputs from 1 to n
+//         while ( mAtomicInt <= n )
+//         {
+//             if ( mAtomicInt % 3 == 0 && mAtomicInt % 5 != 0 )
+//             {
+//                 printFizz();
+//                 mAtomicInt++;
+//             }
+//             else
+//             {
+//                 // Want atomic
+//                 std::this_thread::yield();
+//             }
+//         }
+//     } // end of fizz
+
+//     // printBuzz() outputs "buzz".
+//     void buzz( function<void()> printBuzz ) {
+//         while ( mAtomicInt <= n )
+//         {
+//             if ( mAtomicInt % 3 != 0 && mAtomicInt % 5 == 0 )
+//             {
+//                 printBuzz();
+//                 mAtomicInt++;
+//             }
+//             else
+//             {
+//                 std::this_thread::yield();
+//             }
+//         }
+//     } // end of buzz
+
+//     // printFizzBuzz() outputs "fizzbuzz".
+// 	void fizzbuzz( function<void()> printFizzBuzz ) {
+//         while ( mAtomicInt <= n )
+//         {
+//             if ( mAtomicInt % 15 == 0 )
+//             {
+//                 printFizzBuzz();
+//                 mAtomicInt++;
+//             }
+//             else
+//             {
+//                 std::this_thread::yield();
+//             }
+//         }
+//     } // end of fizzbuzz
+
+//     // printNumber(x) outputs "x", where x is an integer.
+//     void number( function<void( int )> printNumber ) 
+//     {
+//         while ( mAtomicInt <= n )
+//         {
+//             if ( mAtomicInt % 3 != 0 && mAtomicInt % 5 != 0 )
+//             {
+//                 printNumber( mAtomicInt );
+//                 mAtomicInt++;
+//             }
+//             else
+//             {
+//                 std::this_thread::yield();
+//             }
+//         }
+//     } // end of number
+// }; // end of Solution class
