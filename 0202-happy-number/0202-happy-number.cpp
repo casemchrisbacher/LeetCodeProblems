d19f77cf590a56908cc106b6d1ac6cc3ad0826b6
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-05-03 16:16:10 -0600

    Time: 0 ms (100%), Space: 8 MB (24.88%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isHappy( int n )
     {
-        int one_digit{0};
-        std::unordered_set<int> seen_val;
+        int one_digit{0};                   // end digit of number
+        std::unordered_set<int> seen_val;   // marks values as seen to prevent loop
 
+        // end conditions given in problem - ( n == 1), starts looping
         while ( n != 1 && !seen_val.count(n) )
         {
-            int sum{0};
-            seen_val.insert( n );
+            int sum{0};             // sum of the current set
+            seen_val.insert( n );   // insert value as seen
             while ( n > 0 )
             {
                 one_digit = n % 10;
@@ -17,6 +18,6 @@ public:
             }
             n = sum;
         }
-        return ( n == 1 );
+        return ( n == 1 );  // true if happy number, false otherwise
     }
 };
