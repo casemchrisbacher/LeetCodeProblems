5ce4b929a51a093cb1c2c4084b0cf3d57151008b
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-05-10 07:29:07 -0600

    Time: 8 ms (41.8%), Space: 17.3 MB (80.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -4,27 +4,25 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val( 0 ), left( nullptr ), right( nullptr ) {}
- *     TreeNode( int nodeVal ) : val( nodeVal ), left( nullptr ), right( nullptr ) {}
- *     TreeNode( int nodeVal, TreeNode *left, TreeNode *right ) : val( nodeVal ), left( left ), right( right ) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxDepth( TreeNode* root ) {
-        
-        // When it gets past the lowest depth, end recursion
+    // DFS looking for the lowest depth
+    int maxDepth( TreeNode* root )
+    {
+        // base case
         if ( root == nullptr )
         {
             return 0;
         }
-        
-        // Recursively go as far down as possible
-        int left_depth = maxDepth( root->left );    // Go as far down left as possible
-        int right_depth = maxDepth( root->right );  // Go as far down right as possible
-        
-        // Return the maximum of each side, adding one for the current node
-        return ( 1 + max( left_depth, right_depth ) );
-                
+        // get to the bottom, inc return by one every time
+        int left = maxDepth( root->left ) + 1;
+        int right = maxDepth( root-> right ) + 1;
+
+        return max( left, right );
     } // end of maxDepth
 }; // end of Solution class
