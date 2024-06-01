a5a092ed49fb0e9cf227254584659041b3a41c89
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-06-01 09:21:47 -0600

    Time: 5 ms (24.35%), Space: 11.8 MB (44.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,12 +11,14 @@
  */
 class Solution {
 public:
+    // DFS to make sure trees are the same
     bool isSameTree( TreeNode* p, TreeNode* q )
     {
-        if ( p == nullptr && q == nullptr ) return true;
-        if ( p == nullptr || q == nullptr ) return false;
-        if ( p->val != q-> val ) return false;
+        if ( p == nullptr && q == nullptr ) return true;    // both nullptr, still identical
+        if ( p == nullptr || q == nullptr ) return false;   // one nullptr, not identical
+        if ( p->val != q-> val ) return false;              // values don't match
 
+        // all have to return true to match
         return isSameTree( p->right, q->right ) && isSameTree( p->left, q->left );
-    }
-};
+    } // end of isSameTree
+}; // end of SOlution class
