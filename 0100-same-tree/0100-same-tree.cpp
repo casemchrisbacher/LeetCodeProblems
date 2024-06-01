2998e84ec203d1a01bf81333946f6ef40a713f5e
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-06-01 09:20:43 -0600

    Time: 0 ms (100%), Space: 11.9 MB (44.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,25 +11,12 @@
  */
 class Solution {
 public:
-    // DFS, checking each node as we go
-    bool isSameTree( TreeNode* p, TreeNode* q ) 
+    bool isSameTree( TreeNode* p, TreeNode* q )
     {
-        // If both nodes are null, return true
-        if ( p == nullptr && q == nullptr )
-        {
-            return true;
-        }
-        // If one is null and the other isn't, return false (miscompare)
-        if ( p == nullptr || q == nullptr )
-        {
-            return false;
-        }
-        // If the values within the nodes aren't equal, return false
-        if ( p->val != q->val )
-        {
-            return false;
-        }
-        // Pre Order recursive DFS
-        return isSameTree( p->left, q->left ) && isSameTree( p->right, q->right );
-    } // end of isSameTree
-};  // end of Solution class
+        if ( p == nullptr && q == nullptr ) return true;
+        if ( p == nullptr || q == nullptr ) return false;
+        if ( p->val != q-> val ) return false;
+
+        return isSameTree( p->right, q->right ) && isSameTree( p->left, q->left );
+    }
+};
