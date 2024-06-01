0bb25aa8854ebb7b51e017dd6becd8bef85cfe85
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-06-01 10:36:43 -0600

    Time: 0 ms (100%), Space: 9 MB (48.61%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome( string s )
+    {
+        std::unordered_map<char,int> letters;
+        int num_in_pal{ 0 };
+
+        // add the letters to map
+        for ( const char & letter : s )
+        {
+            letters[ letter ]++;
+        }
+
+        // loop through to count the pairs
+        for ( const auto & char_count_pair : letters )
+        {
+            num_in_pal += char_count_pair.second - ( char_count_pair.second % 2 );
+        }
+        return num_in_pal + !( num_in_pal == s.length() );
+    }
+};
