ab9949dd46573d93173a06476128b3b8577ad926
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-06-01 09:17:53 -0600

    Time: 5 ms (49.3%), Space: 9 MB (55.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
     {
         std::vector<int> bits( n+1 );
         if ( n == 0 ) return bits;
-        
+
         bits[ 1 ] = 1;
 
         for ( int ind = 2; ind <= n; ind++ )
@@ -12,5 +12,5 @@ public:
             bits[ ind ] = ( ind % 2 ) + bits[ ind / 2 ];
         }
         return bits;
-    }
-};
+    } // end of countBits
+}; // end of Solution class
