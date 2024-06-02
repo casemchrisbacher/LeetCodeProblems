5c7e7d360a4c893d3a61368cd66ba4fbe005d142
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-06-02 11:00:11 -0600

    Time: 0 ms (100%), Space: 9.7 MB (89.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -9,17 +9,18 @@ public:
 
         for ( int ind = 0; ind < S_LEN; ind++ )
         {
+            // use the ASCII as integers for indexing
             letter_cnt[ s[ ind ] ]++;
-        
-            while ( letter_cnt[ s[ ind ] ] > 1 )
+
+            // if there is more than one of that letter in vector
+            // pop letters off the back of the vector until there is but one
+            while ( letter_cnt[ s[ ind ] ] > 1 ) 
             {
                 letter_cnt[ s[ slow ] ]--;
                 slow++;
             }
-
             max = std::max( ind - slow + 1, max );
         }
         return max;
-        
-    }
-};
+    } // end of lengthOfLongestSubstring
+}; // end of Solution class
