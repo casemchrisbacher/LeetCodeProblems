68c7e66ba749fcf5048110a5314b3ac20d71ed58
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-07-07 08:38:37 -0600

    Time: 12 ms (59.24%), Space: 13.9 MB (38.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum( vector<int>& nums, int target ) 
+    vector<int> twoSum(vector<int>& nums, int target)
     {
-        unordered_map<int,int> val_index_map;   // map: value, index
+        std::unordered_map<int,int> nums_map; // key: number, value: index
+        const int NUM_SIZE = nums.size();
 
-        // Loop through index
-        for ( int index = 0; index < nums.size(); index++ )
+        for ( int i = 0; i < NUM_SIZE; i++ )
         {
-            // The pair value of the one at the curent index
-            int complement = target - nums[ index ];
-
-            // check if anything has been added at that spot
-            if ( val_index_map.count( complement ) )
+            if ( nums_map.count( target - nums[i] ) )
             {
-                // max ONE solution
-                return { val_index_map[ complement ], index };
+                return { nums_map[ target - nums[i] ], i };
             }
-            val_index_map[ nums[ index ] ] = index;
+            nums_map[ nums[i] ] = i;
         }
-        // Should never hit here - every input has exactly one solution
         return {};
     } // end of twoSum
-}; // end of Solution class
+};
