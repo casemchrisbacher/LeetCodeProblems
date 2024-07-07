18fc66ffb5efed4838f55a6eb1009e4e5cca7ac3
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-07-07 08:45:48 -0600

    Time: 7 ms (84.66%), Space: 13.9 MB (38.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        std::unordered_map<int,int> nums_map; // key: number, value: index
-        const int NUM_SIZE = nums.size();
+        std::unordered_map<int, int> nums_map; // key: number, value: index (of value in nums vector)
+        const int SIZE_NUMS = nums.size();
 
-        for ( int i = 0; i < NUM_SIZE; i++ )
+        for ( int i = 0; i < SIZE_NUMS; i++ )
         {
+            // x + y = target
+            // assume current value is x, AKA x = nums[i]
+            // rearrange function to: y = target - x
+            // if we have previously stored that value (y) in nums_map, we have a pair
+            // the problem asks for indexes, which is the value stored at y and current i
             if ( nums_map.count( target - nums[i] ) )
             {
                 return { nums_map[ target - nums[i] ], i };
             }
-            nums_map[ nums[i] ] = i;
+            nums_map[ nums[i] ] = i; // else: add it to the map and keep on lookin
         }
         return {};
     } // end of twoSum
