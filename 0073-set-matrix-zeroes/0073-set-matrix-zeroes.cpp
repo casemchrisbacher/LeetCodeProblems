bb723998922dc2c416f8d3dda349d37d63288545
casemchrisbacher <142862865+casemchrisbacher@users.noreply.github.com>
2024-07-13 14:24:10 -0600

    Time: 8 ms (76.83%), Space: 16.8 MB (13.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,27 +2,74 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        std::vector<std::pair<int, int>> inds;
+        bool top_row{false};
+        bool left_col{false};
+
         for ( int y = 0; y < matrix.size(); y++ )
         {
-            for ( int x = 0; x < matrix[0].size(); x++ )
+            if ( matrix[y][0] == 0 )
+            {
+                left_col = true;
+            }
+        }
+
+        for ( int x = 0; x < matrix[0].size(); x++ )
+        {
+            if ( matrix[0][x] == 0 )
+            {
+                top_row = true;
+            }
+        }
+
+        for ( int y = 1; y < matrix.size(); y++ )
+        {
+            for ( int x = 1; x < matrix[0].size(); x++ )
             {
                 if ( matrix[y][x] == 0 )
                 {
-                    inds.push_back({y, x});
+                    matrix[0][x] = 0;
+                    matrix[y][0] = 0;
                 }
             }
         }
-        for ( int i = 0; i < inds.size(); i++ )
+
+        for ( int y = 1; y < matrix.size(); y++ )
+        {
+            if ( matrix[y][0] == 0 )
+            {
+                for ( int x = 0; x < matrix[0].size(); x++ )
+                {
+                    matrix[y][x] = 0;
+                }
+            }
+        }
+
+        for ( int x = 1; x < matrix[0].size(); x++ )
+        {
+            if ( matrix[0][x] == 0 )
+            {
+                for ( int y = 0; y < matrix.size(); y++ )
+                {
+                    matrix[y][x] = 0;
+                }
+            }
+        }
+        
+        if ( left_col )
         {
             for ( int y = 0; y < matrix.size(); y++ )
             {
-                matrix[y][inds[i].second] = 0;
-            }
-            for ( int x = 0; x < matrix[0].size(); x++ )
-            {
-                matrix[inds[i].first][x] = 0;
+                matrix[y][0] = 0;
             }
         }
+        if ( top_row )
+        {
+            for ( int x = 0; x < matrix[0].size(); x++ )
+            {
+                matrix[0][x] = 0;
+            }
+        }
+
+
     }
 };
